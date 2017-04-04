976ce15c57aa5b3719b72d8858213e6260e69477
ChenJiaXin <mun.beatrice@gmail.com>
2017-04-04 12:22:11 +0800

    modify with ticker

--- main.cpp
+++ main.cpp
@@ -3,25 +3,29 @@
 
  I2C m_i2c(D14,D15);
  char m_addr = 0x90;
+ Ticker TmpOnLCD;
  
  TextLCD lcd(D2, D3, D4, D5, D6, D7);
+ 
+
+ void displayLCD(){
+   
+   const char tempRegAddr = 0x00;
+   m_i2c.write(m_addr, &tempRegAddr, 1); //Pointer to the temperature register
+   char reg[2] = {0,0};
+   m_i2c.read(m_addr, reg, 2); //Read
+   unsigned short res = (reg[0] << 4) | (reg[1] >> 4);
+   float temp =  (float) ((float)res * 0.0625);
+         
+   lcd.locate(2,1);
+   lcd.printf("Temp: %2.2f C", temp);     
+ 
+ }
 
  int main()
 
  {
      lcd.cls();
-     while(1) {
-
-         const char tempRegAddr = 0x00;
-         m_i2c.write(m_addr, &tempRegAddr, 1); //Pointer to the temperature register
-         char reg[2] = {0,0};
-         m_i2c.read(m_addr, reg, 2); //Read
-         unsigned short res = (reg[0] << 4) | (reg[1] >> 4);
-         float temp =  (float) ((float)res * 0.0625);
-         
-         lcd.locate(2,1);
-         lcd.printf("Temp: %2.2f C", temp);
-         wait(1.0);
-     }
+     TmpOnLCD.attach(&displayLCD, 1.0);
 
  }
